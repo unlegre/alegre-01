aec51bdb3fe9be96163c0b5384bf659e84677e93
“gabriel <unlegre@gmail.com>
2022-08-28 17:31:02 -0300

    se agrego navbar con stilos css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import {AiOutlineShoppingCart} from 'react-icons/ai';
+import { NavBar } from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
