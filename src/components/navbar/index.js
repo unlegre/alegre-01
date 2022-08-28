bc19c2e1f4b37930fccdd3959ffefdf48805cc88
“gabriel <unlegre@gmail.com>
2022-08-28 16:45:36 -0300

    se agrego navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <ul>
+        <li>
+            <a href="/#">home</a> 
+        </li>
+        <li>
+            <a href="/#">News</a> 
+        </li>
+        <li>
+            <a href="/#">Contact</a> 
+        </li>
+        <li>
+            <a href="/#">Abaut</a> 
+        </li>
+    </ul>
+  );
+};
